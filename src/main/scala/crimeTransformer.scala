fd38229145e2399b4015d01de644b63c849e60b8
Collin Wong <cwong109@calpoly.edu>
2018-12-09 21:43:04 -0800

    Added comments to code

--- src/main/scala/crimeTransformer.scala
+++ src/main/scala/crimeTransformer.scala
@@ -32,6 +32,7 @@ object crimeTransformer {
     val incomeLines = sc.textFile("resources/income_LA.csv")
       .map(line => line.split(", "))
       .map(split => ((split(0).toDouble, split(1).toDouble), split(2).toDouble.round))
+    // ((lat, long), averageIncome)
 
     //val crimeLines = sc.textFile("D:\\Eclipse Workspace\\LosAngelesCrime\\src\\Data\\Crime_Data_from_2010_to_Present.csv")
     val crimeLines = sc.textFile("resources/crime.csv")
@@ -42,47 +43,57 @@ object crimeTransformer {
       .map({case ((long, lat), line) => ((parseDouble(long.substring(2)), parseDouble(lat.substring(0, lat.size - 2))), line)})
       .filter(_._1._1 != None)
       .filter(_._1._2 != None)
+      // if we want to map to lower tenth:
       //.map({case ((Some (long), Some (lat)), line) => (((long.toDouble * 10).round / 10.0, (lat.toDouble * 10).round / 10.0), line)})
-
+      // if we want to map to lower 0.05
       .map({case ((Some (long), Some (lat)), line) => ((mutateCoords(long), mutateCoords(lat)), line)})
+
 
     // now have all the ((lat, long), lineWithDetail) in the RDD
     // can just map to whatever split we want
-    // TO GROUP AND SORT: crimeLines.groupByKey().sortBy(_._1)
+    //   TO GROUP AND SORT: crimeLines.groupByKey().sortBy(_._1)
+    // TO JOIN WITH INCOME: (...).join(incomeLines)
 
 
-     // .join(incomeLines).count()
 
-//    // crimeCodes has each (long, lat) pair with each (code, count) pair for every code found in that grid square
-//    val crimeCode = crimeLines.map({case (coord, line) => (coord, line.split(",")(7))})
-//      .filter({case (coord, code) => code != ""})
-//      // have ((lat, long), crimeCode)
-//      .map((_, 1))
-//      .reduceByKey(_+_)
-//      .map({case (((lat, long), code), count) => ((lat, long), (code, count))})
-//      .sortByKey()
-//
-//    //    val pw1 = new PrintWriter(new File("D:\\Eclipse Workspace\\LosAngelesCrime\\src\\FixedData\\crime_Codes.csv"))
-//    val pw1 = new PrintWriter(new File("resources/crime_Codes.csv"))
-//    crimeCode.collect().foreach({case ((long, lat), (code, count)) =>
-//      pw1.write(long.toString + ", " + lat.toString + ", " + code + ": " + count + "\n")})
-//    pw1.close()
+    // crimeCodes has each (long, lat) pair with each (code, count) pair for every code found in that grid square
+    val crimeCode = crimeLines.map({case (coord, line) => (coord, line.split(",")(7))})
+      .filter({case (coord, code) => code != ""})
+      // have ((lat, long), crimeCode)
+      .map((_, 1))
+      .reduceByKey(_+_)
+      .map({case (((lat, long), code), count) => ((lat, long), (code, count))})
+      .sortByKey()
+    // ((lat, long), (crimeCode, crimeCount)) sorted by lat and long
+
+    //    val pw1 = new PrintWriter(new File("D:\\Eclipse Workspace\\LosAngelesCrime\\src\\FixedData\\crime_Codes.csv"))
+    val pw1 = new PrintWriter(new File("resources/crime_Codes.csv"))
+    crimeCode.collect().foreach({case ((long, lat), (code, count)) =>
+      pw1.write(long.toString + ", " + lat.toString + ", " + code + ": " + count + "\n")})
+    pw1.close()
 
 
     // numCodes has number of crimes in each (long, lat) pair
     val numCrimes = crimeLines.map({case (coord, line) => coord})
       // have (lat, long)
       .map((_, 1))
+      // now ((lat, long), 1)
       .reduceByKey(_+_)
+      // get number of crimes per grid square
       .join(incomeLines)
       .filter(_._2._1 > 100)
+      // filter any grid squares with < 100 crimes
       .sortBy(_._2._2)
+      // sort by income
 
-    //    val pw2 = new PrintWriter(new File("D:\\Eclipse Workspace\\LosAngelesCrime\\src\\FixedData\\crime_Counts.csv"))
+    // val pw2 = new PrintWriter(new File("D:\\Eclipse Workspace\\LosAngelesCrime\\src\\FixedData\\crime_Counts.csv"))
     val pw2 = new PrintWriter(new File("resources/crime_Counts.csv"))
+
     numCrimes.collect.foreach({case ((long, lat), (count, income)) =>
-//      pw2.write(long.toString + ", " + lat.toString + ":: Income: " + income + ", Count:" + count + "\n")})
-      pw2.write(income + "," + count + "\n")})   // FOR CSV FILE USAGE
+      pw2.write(long.toString + ", " + lat.toString + ":: Income: " + income + ", Count:" + count + "\n")
+      // pw2.write(income + "," + count + "\n")})   // FOR CSV FILE USAGE
+
+    })
     pw2.close()
 
 
@@ -91,16 +102,7 @@ object crimeTransformer {
   // splitting coords to more than 57 (if we want more)
   // method will round all the coords down to the lower 0.05
   def mutateCoords (x : Double): Double = {
-
     val rounded = (x * 100).round
     return (rounded - (rounded % 5)) / 100.0
-
-
-    /*val y = (BigDecimal(x).setScale(2, BigDecimal.RoundingMode.HALF_DOWN) * 100).toInt
-    val z = BigDecimal(x).setScale(2, BigDecimal.RoundingMode.HALF_DOWN)
-    //      val y = (x - BigDecimal(x).setScale(0, BigDecimal.RoundingMode.DOWN).toDouble * 100).round
-    y match {
-      case it if 0 until 2 contains it => 0.00
-    }*/
   }
 }
--- src/main/scala/incomeTransformer.scala
+++ src/main/scala/incomeTransformer.scala
@@ -22,27 +22,31 @@ object incomeTransformer {
     Logger.getLogger("akka").setLevel(Level.OFF)
     val conf = new SparkConf().setAppName("NameOfApp").setMaster("local[4]")
     val sc = new SparkContext(conf)
-    //val pw = new PrintWriter(new File("D:\\Eclipse Workspace\\LosAngelesCrime\\src\\FixedData\\income_LA.csv"))
-    val pw = new PrintWriter(new File("resources/income_LA.csv"))
+
 
     //val incomeLines = sc.textFile("D:\\Eclipse Workspace\\LosAngelesCrime\\src\\Data\\Income__LA_.csv")
     val incomeLines = sc.textFile("resources/income.csv")
       .map(line => line.split("\""))
       .filter(_.size == 7)
       .map(split => (split(1), split(5)))
+      // (income, coordinates)
       .filter(_._1 != "")
       .filter(_._2 != "")
+      // make sure input is all good
       .map({case (income, coords) => (income.split(",").mkString("").toInt,
         coords.split(",")(0).substring(1).toDouble,
         coords.split(", ")(1).dropRight(1).toDouble)})
+      // if we want to map to lower tenth:
       //.map({case (income, long, lat) => (((long * 10).round / 10.0,(lat * 10).round / 10.0), income)})
+      // if we want to map to lower 0.05:
       .map({case (income, long, lat) => ((mutateCoords(long), mutateCoords(lat)), income)})
       .aggregateByKey((0,0))((x, y) => (x._1 + y, x._2 + 1), (x, y) => (x._1 + y._1, x._2 + y._2))
-      //.map({case ((long, lat), incomeList) => (long, lat, incomeList.reduce(_+_)/incomeList.size)})
+      // aggregate the income of every similar lat/long pair
       .sortBy(_._1)
 
 
-
+    //val pw = new PrintWriter(new File("D:\\Eclipse Workspace\\LosAngelesCrime\\src\\FixedData\\income_LA.csv"))
+    val pw = new PrintWriter(new File("resources/income_LA.csv"))
     incomeLines.collect().foreach({case ((long, lat), (sum, count)) =>
       pw.write(long.toString + ", " + lat.toString + ", " + (sum*1.0/count).toString + ", " + count + "\n")})
     pw.close()
@@ -54,12 +58,5 @@ object incomeTransformer {
 
     val rounded = (x * 100).round
     return (rounded - (rounded % 5)) / 100.0
-
-    /*val y = (BigDecimal(x).setScale(2, BigDecimal.RoundingMode.HALF_DOWN) * 100).toInt
-    val z = BigDecimal(x).setScale(2, BigDecimal.RoundingMode.HALF_DOWN)
-    //      val y = (x - BigDecimal(x).setScale(0, BigDecimal.RoundingMode.DOWN).toDouble * 100).round
-    y match {
-      case it if 0 until 2 contains it => 0.00
-    }*/
   }
 }
